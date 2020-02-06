de42d995384e96c8326a5acdd1297342004ba413
Scott Rothman <scottrothman@Scotts-MacBook-Air.local>
2020-02-06 21:27:50 +0900

    Basic implementation of maquee element working

---
+++ index.js
@@ -0,0 +1,47 @@
+class MMarquee extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({mode: 'open'});
+    const wrapper = document.createElement('div');
+    const text = document.createElement('span');
+
+    wrapper.classList.add('wrapper');
+    text.classList.add('text');
+
+    text.innerText = this.innerHTML;
+
+    const duration = text.innerText.length * .2;
+
+    const styles = document.createElement('style');
+    styles.textContent = `
+      .wrapper {
+        display: inline-block;
+        overflow: hidden;
+      }
+
+      .text {
+        animation: marquee ${duration}s linear;
+        animation-iteration-count: infinite;
+        position: relative;
+      }
+
+      @keyframes marquee {
+        from {
+          left: -100%;
+        }
+
+        to {
+          left: 100%;
+        }
+      }
+
+    `;
+
+    wrapper.classList.add('wrapper');
+    wrapper.appendChild(text);
+    shadow.appendChild(styles);
+    shadow.appendChild(wrapper);
+  }
+}
+
+customElements.define('m-marquee', MMarquee);
